82bf62a447b3fdb5889160831797a8c5d41d7387
muntazif <04190000633@ogrenci.ege.edu.tr>
2022-05-30 15:14:50 +0300

    First commit

---
+++ MobilyaDukkani/View/MainView.swift
@@ -0,0 +1,68 @@
+//
+//  MainView.swift
+//  MobilyaDukkani
+//
+//  Created by İbrahim Güler on 30.05.2022.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    @StateObject var appModel : AppViewModel = .init()
+    @Namespace var animation
+    
+    init() {
+        UITabBar.appearance().isHidden = true
+    }
+    
+    var body: some View {
+        TabView(selection: $appModel.currentTab) {
+            Home(animation: animation)
+                .environmentObject(appModel)
+                .tag(Tab.home)
+                .setUpTab()
+            
+            Text("Cart")
+                .tag(Tab.cart)
+                .setUpTab()
+            
+            Text("Favourite")
+                .tag(Tab.favourite)
+                .setUpTab()
+            
+            Text("Profile")
+                .tag(Tab.profile)
+                .setUpTab()
+            
+        }
+        .overlay(alignment: .bottom) {
+            CustomTabBar(currentTab: $appModel.currentTab, animation: animation)
+                .offset(y: appModel.showDetailView ? 150 : 0)
+        }
+        .overlay {
+            if let furniture = appModel.currentActiveItem, appModel.showDetailView {
+                DetailView(furniture: furniture, animation: animation)
+                    .environmentObject(appModel)
+                    .transition(.offset(x:1, y:1))
+            }
+        }
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
+
+extension View {
+    @ViewBuilder
+    func setUpTab() -> some View {
+        self
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background {
+                Color("BG")
+                    .ignoresSafeArea()
+            }
+    }
+}
